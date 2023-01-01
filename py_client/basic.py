8760e07eaecb29cd14dea43421346ae021e4bfe4
Ramshiv7 <Ramshiv7@users.noreply.github.com>
2023-01-01 22:21:15 +0530

    Initial Setup

---
+++ py_client/basic.py
@@ -0,0 +1,25 @@
+import requests 
+
+endpoint = "https://www.github.com"
+
+#requests.get()
+
+get_response = requests.get(endpoint)
+
+print(get_response.text)
+# if improper api end point - then HTML will be returned 
+# if End point is proper - then JSON Data is returned 
+
+# Application Programming Inteface 
+
+# REST API -> Web API 
+
+# API - Application progamming interface 
+
+# Request and Response 
+
+# Get, POST, PUT,  
+
+# CRUD - Create,Read, Update, Delete
+
+
